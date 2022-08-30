a46709b128cb30caf97ae41a2423ca657e6a6b81
João Pinto <joaopintojpgp@gmail.com>
2022-08-30 21:27:47 +0000

    add lsp error and warning symbols

--- lua/nvim-lspconfig.lua
+++ lua/nvim-lspconfig.lua
@@ -8,7 +8,13 @@ end
 -- Language servers
 local nvim_lsp = require('lspconfig')
 local servers = { 'rust_analyzer' } -- Add servers here
-
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup {}
+end
+
+-- Diagnostics signs on the line number column
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl= hl, numhl = hl })
 end
